835f468eaa80d038e7d3aa3b060d1e6866dc8cc0
Artem <beliko.artem43334@icloud.com>
2025-05-04 19:36:10 +0300

    Покращення
    Виправлено навігацію для маленьких екранів, виправлено секцію Skills, виправлено footer, додано кнопку Back To Top(при натисканні повертає користувача наверх), також багато дрібних помилок

---
+++ dist/backToTop.dev.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var backtotop_btn = document.getElementById("backtotop-btn");
+
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 700 || document.body.scrollTop > 700) {
+    backtotop_btn.setAttribute("class", "slide-in-bottom");
+    backtotop_btn.style.visibility = "visible";
+  } else {
+    backtotop_btn.setAttribute("class", "slide-out-top");
+    backtotop_btn.style.visibility = "hidden";
+  }
+};
+
+backtotop_btn.onclick = function () {
+  document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0;
+};
